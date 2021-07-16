67f6792ca35ce39d34ac1c9550232727c3c90ee3
AdityaSanthosh <pssanthoshaditya2000@gmail.com>
2021-07-16 13:48:12 +0530

    Sleep Detection of Truck Drivers

---
+++ Recognize.py
@@ -0,0 +1,67 @@
+from scipy.spatial import distance
+import face_recognition
+import cv2
+
+
+def recognize_sleep():
+    eyes_closed_time = 0
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cap.read()
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
+        face_landmarks_list = face_recognition.face_landmarks(rgb_small_frame)
+        current_face_locations = face_recognition.face_locations(rgb_small_frame)
+        current_face_encodings = face_recognition.face_encodings(rgb_small_frame, current_face_locations)
+
+        for face_landmark in face_landmarks_list:
+            left_eye = face_landmark['left_eye']
+            right_eye = face_landmark['right_eye']
+            ear_left = get_ear(left_eye)
+            ear_right = get_ear(right_eye)
+            closed = ear_left < 0.2 and ear_right < 0.2
+            if closed:
+                eyes_closed_time += 1
+            if not closed:
+                eyes_closed_time = 0
+            for (top, right, bottom, left), face_encoding in zip(current_face_locations, current_face_encodings):
+                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+                top *= 4
+                right *= 4
+                bottom *= 4
+                left *= 4
+                if eyes_closed_time > 4:
+                    # Draw a Rectangle around the Face
+                    cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+                    cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+                    font = cv2.FONT_HERSHEY_DUPLEX
+                    cv2.putText(frame, "Sleeping", (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+                if eyes_closed_time < 4:
+                    # Draw a Rectangle around the Face
+                    cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+                    cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+                    font = cv2.FONT_HERSHEY_DUPLEX
+                    cv2.putText(frame, "Not Sleeping", (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+        # display the frame
+        cv2.imshow('Video', frame)
+        # wait for 100 milliseconds
+        if cv2.waitKey(100) & 0xFF == ord('q'):
+            print('exited..\n')
+            cap.release()
+            cv2.destroyAllWindows()
+            break
+
+
+def get_ear(eye):
+    A = distance.euclidean(eye[1], eye[5])
+    B = distance.euclidean(eye[2], eye[4])
+    C = distance.euclidean(eye[0], eye[3])
+    ear = (A + B) / (2.0 * C)
+    return ear
+
+
+recognize_sleep()
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Sleep Detection of Truck Drivers
+
+Used Face Landmarks in dlib library to detect if a person is sleeping.
+And that is done by calculating the amount of time an eye is close.
